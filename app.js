525fd43b1cc569a0dc4020cb9f44b158131ca6e3
k17h02 <k17h02@gmail.com>
2021-09-15 22:30:59 +0600

    js clean

--- app.js
+++ app.js
@@ -1,44 +1,66 @@
-const monthlyIncomeButton =()=>{
+// inner text set 
+const setInnerText = (id, value) => {
+    document.getElementById(id).innerText = value;
+    console.log("inner text set")
 
-    const monthlyIncome = document.getElementById('monthly-income-input')
-    const monthlyIncomeValue = monthlyIncome.value;
-    monthlyIncome.value = ""
-
-  
-
-    const monthlyIncomeSet = document.getElementById('monthly-income');
-    const  oldMonney = monthlyIncomeSet.innerText;
-    console.log(oldMonney)
-    monthlyIncomeSet.innerText= parseInt(monthlyIncomeValue) + parseInt(oldMonney);
 }
-const addCost=()=>{
-    const costDtails = document.getElementById("cost-dtails")
-    const costAmount = document.getElementById("cost-amount")
 
-    const costDtailsValue = costDtails.value;
-    const costAmountValue = costAmount.value;
-    costDtails.value=" ";
-    costAmount.value= " ";
-   
-//  li create and cost add 
+//  input value 
+const getValue = id => {
+    const gId = document.getElementById(id);
+    const gvalue = parseInt(gId.value);
+    return gvalue;
+}
+//  iner tex  
+const getInnerValu = id => {
+    const getId = document.getElementById(id)
+    const idValue = getId.innerText;
+    const value = parseInt(idValue);
+    return value;
+    // console.log(value)
+    // if (isNaN(value)) {
+    //     console.log("wrong input")
+    //     return 1111;
+    // }
+    // else {
+    //     return value;
+    // }
+}
+//  add  li to  ul 
+function addLI(id, amount) {
+    console.log(id)
     const li = document.createElement('li');
     li.classList.add('list-group-item', 'd-flex', 'justify-content-between')
-    li.innerHTML=`<p>${costDtailsValue}</p> <p>= ${costAmountValue} tk</p>`
+    li.innerHTML = `<p>${id}</p> <p>= ${amount} tk</p>`
     document.getElementById("ul-parrent").appendChild(li);
 
-    const oldTotalCost = document.getElementById("total-cost").innerText
-    const newTotalCost = parseInt(costAmountValue)+parseInt(oldTotalCost);
-    document.getElementById("total-cost").innerText = newTotalCost;
+}
 
-    
-
-
-    document.getElementById('li-total-cost').innerText =newTotalCost;
-
-   
-    const totalMoney = document.getElementById ('monthly-income').innerText
-    console.log( parseInt(totalMoney))
-const availableMoeny = parseInt(totalMoney) -newTotalCost;
-
-document.getElementById('availeble-money').innerText= availableMoeny;
+// monthly income button 
+const monthlyIncomeButton = () => {
+    const newMonthlyIncome = getValue('monthly-income-input')
+    const oldMonney = getInnerValu('monthly-income');
+    const netIncome = newMonthlyIncome + oldMonney;
+    if (!isNaN(netIncome)) {
+        setInnerText('monthly-income', netIncome)
+        const alMoney = getInnerValu('monthly-income') - getInnerValu('total-cost');
+        console.log(alMoney)
+        setInnerText('availeble-money', alMoney)
+        document.getElementById('monthly-income-input').value = '';
+    }
+}
+// cost dtails 
+const addCost = () => {
+    const costDtails = document.getElementById("cost-dtails").value;
+    const newCost = getValue("cost-amount")
+    if (costDtails && !isNaN(newCost)) {
+        addLI(costDtails, newCost);
+        const oldTotalCost = getInnerValu('li-total-cost')
+        const newTotalCost = newCost + oldTotalCost;
+        setInnerText('total-cost', newTotalCost)
+        setInnerText('li-total-cost', newTotalCost)
+        const totalMoney = getInnerValu('monthly-income')
+        const availableMoeny = totalMoney - newTotalCost;
+        setInnerText('availeble-money', availableMoeny)
+    }
 }
